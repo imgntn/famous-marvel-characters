cb688046958f28551ef3ddffc9b97e3b8f5e0794
James B. Pollack <jamesbradenpollack@gmail.com>
2014-04-28 01:02:28 -0700

    compression

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 var express = require('express');
+var compress = require('compression')
 var app = express();
- app.use(express.compress());
+app.use(compress())
 app.use(express.static(__dirname + '/dist'));
-
 app.listen(process.env.PORT || 5000);
