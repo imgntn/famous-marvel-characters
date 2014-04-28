85493d33656a2d1bd3e5c355538151fbd254510c
James B. Pollack <jamesbradenpollack@gmail.com>
2014-04-28 01:08:00 -0700

    error handling causes errors.  sigh.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var compress = require('compression')
-var errorHandler = require('errorhandler')
+
 var app = express();
-app.use(errorHandler);
+
 app.use(compress())
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
